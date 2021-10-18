3d0ed231b4b5ceb2f95bf326675d4fe81538877b
Alan T <55752172+alanctnk@users.noreply.github.com>
2021-10-18 07:21:58 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <Content>
           <div className="p-5 mb-4 rounded-3">
             <div className="container-fluid py-5">
-              <h1 className="display-5 fw-bold text-liverdogs">Welcome to DRINKLY,</h1>
+              <h1 className="display-5 fw-bold text-liverdogs">Welcome to DRINKLY</h1>
               <p className="col-md-8 fs-4">An open source application with a large database information about drinks. You can list by category, search by name, set favorite and see details for a drink.</p>
               <Link className="btn btn-success btn-lg" to="/drinks">GET STARTED</Link>
             </div>           
